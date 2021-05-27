3e0c09451a1906a0bae4de7d187043d820555c08
priyanka676 <84844504+priyanka676@users.noreply.github.com>
2021-05-27 02:10:34 -0700

    Add files via upload

---
+++ day1/home.php
@@ -0,0 +1,29 @@
+
+  <?php
+$title = "Home";
+
+
+$content = '<img src="image2.jpg"  height="600" width="600" class="imgLeft" />
+<h1>HOSPITAL MANAGEMENT SYSTEM</h1>
+<p>
+
+    Hospital Management is all about the planning, implementation, coordination, and health care facilities.
+ 
+ <br/>
+ 
+      Hospitals play a very important role in the delivery of healthcare services. 
+   
+   </p>
+   
+   <br/>
+   <p>
+   
+   	The design and all the facilities and  information are stand alone or in an integrated environment.
+	
+	</p>
+</p>';
+
+
+include 'Design.php';
+?>
+
---
+++ day1/signup.php
@@ -0,0 +1,31 @@
+<?php
+?>
+<html>
+<head>
+<title>Sign up form</title>
+
+</head>
+<body>
+<div class="container">
+<div class="row">
+<div class ="col-lg-6">
+         <div class="sign-up-form">
+			<h1> Signup form</h1>
+			<form action="home.php" method="post">
+			
+			<div class="form-group">
+			<label>Enter UserName </label>
+			<input type="text" name="user" class="form-control">
+			</div>
+			
+			
+			<div class="form-group">
+			<label>Enter Password </label>
+			<input type="password" name="password" class="form-control">
+			</div>
+			<button class="btn-btn-primary">login</button>
+			<p>do you have an account ? <a href="#">sign in</a></p>
+			</form>
+			</body>
+			</html>
+			
